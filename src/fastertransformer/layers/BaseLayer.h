755ae2c01c07a62433603392d537a1e35d80d44a
zhouquanwei <1369374470@qq.com>
2023-07-21 17:45:39 +0800

    添加注释

--- src/fastertransformer/layers/BaseLayer.h
+++ src/fastertransformer/layers/BaseLayer.h
@@ -55,14 +55,26 @@ protected:
     virtual void freeBuffer()     = 0;
 
     // device environments
-    cudaStream_t     stream_;
-    cublasMMWrapper* cublas_wrapper_;
-    IAllocator*      allocator_;
-    cudaDeviceProp*  cuda_device_prop_ = nullptr;
+    cudaStream_t     stream_;// 保存 CUDA 流的句柄，用于指定 GPU 执行操作的上下文
+    cublasMMWrapper* cublas_wrapper_; // cublasMMWrapper 类的指针，用于执行矩阵乘法操作。
+    IAllocator*      allocator_; // IAllocator 类的指针，用于在 GPU 上分配和释放内存。
+    cudaDeviceProp*  cuda_device_prop_ = nullptr;//cudaDeviceProp 结构的指针，用于保存 GPU 设备的属性信息。
 
-    bool is_free_buffer_after_forward_;
-    bool is_allocate_buffer_ = false;  // TODO (bhsueh) to be deprecated
-    bool sparse_;
+    bool is_free_buffer_after_forward_;// 表示在前向推断结束后是否释放缓冲区内存。
+    bool is_allocate_buffer_ = false;  // 未来被弃用（deprecated），用于表示是否已经分配缓冲区内存。
+    bool sparse_;//表示是否使用稀疏计算。
 };
 
 }  // namespace fastertransformer
+
+// BaseLayer 在 FasterTransformer 库中的作用包括：
+
+// 提供 CUDA 流（cudaStream_t）：每个 GPU 设备都有一个或多个 CUDA 流，用于指定 GPU 执行操作的上下文。BaseLayer 通过成员变量 stream_ 保存 CUDA 流的句柄，并提供了接口函数 getStream() 和 setStream()，允许其他层类在 GPU 上指定执行操作的流。
+
+// 提供 cuBLAS 封装（cublasMMWrapper）：cuBLAS 是 NVIDIA 提供的用于执行矩阵乘法和其他线性代数运算的库。BaseLayer 通过成员变量 cublas_wrapper_ 保存 cuBLAS 封装类的指针，可以在其他层类中直接调用该封装类的函数来执行矩阵乘法操作。
+
+// 提供内存管理接口（IAllocator）：GPU 上的内存管理是一个重要的任务，用于在 GPU 上分配和释放内存。BaseLayer 通过成员变量 allocator_ 保存内存分配器类的指针，允许其他层类使用该内存分配器在 GPU 上分配和释放内存。
+
+// GPU 设备属性信息：BaseLayer 可以通过成员变量 cuda_device_prop_ 保存 GPU 设备的属性信息，供其他层类查询和使用。
+
+// 管理缓冲区内存：BaseLayer 中的纯虚函数 allocateBuffer() 和 freeBuffer() 定义了缓冲区内存的分配和释放操作。这些函数需要在具体的层类中实现，用于管理不同层之间的临时缓冲区内存
